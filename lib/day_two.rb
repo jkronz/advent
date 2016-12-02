0575ddabcc0ea81ffd0210416e3a07920b6c68cd
Justin Kronz <jkronz@gmail.com>
2016-12-02 10:53:18 -0500

    Day 2, define keyboards with coordinates

--- lib/day_two.rb
+++ lib/day_two.rb
@@ -1,46 +1,59 @@
-class DayOne
-  TOP = [1, 2, 3]
-  BOTTOM = [7, 8, 9]
-  LEFT = [1, 4, 7]
-  RIGHT = [3, 6, 9]
+class DayTwo
+  ONE = [['1', '2', '3'],
+         ['4', '5', '6'],
+         ['7', '8', '9']]
+  TWO = [[nil, nil, '1', nil, nil],
+         [nil, '2', '3', '4', nil],
+         ['5', '6', '7', '8', '9'],
+         [nil, 'A', 'B', 'C', nil],
+         [nil, nil, 'D', nil, nil]]
 
   def initialize
-    @current_pos = 5
     @code = []
   end
 
-  def parse_input(input)
-    input.split('\n').each do |line|
-      @code << parse_line(line.gsub(' ', ''))
+  def parse_input(input, keypad=ONE, current_pos=[1, 1])
+    input.gsub(/\s+/, ' ')
+    @current_pos = current_pos
+    input.split(' ').each do |line|
+      @code << parse_line(line.gsub(' ', ''), keypad)
     end
-    @code.join('')
+    @code.join
   end
 
-  def parse_line(line)
+  def parse_line(line, keypad)
     line.chars.each do |character|
-      send("move_#{character}")
+      send("move_#{character}", keypad)
     end
-    @current_pos
+    keypad[@current_pos[0]][@current_pos[1]]
   end
 
-  def move_L
-    return if LEFT.include?(@current_pos)
-    @current_pos = @current_pos - 1
+  def move_L(keypad)
+    row, column = @current_pos
+    column = column - 1
+    return if column == -1 || keypad[row][column].nil?
+    @current_pos = [row, column]
   end
 
-  def move_R
-    return if RIGHT.include?(@current_pos)
-    @current_pos = @current_pos + 1
+  def move_R(keypad)
+    row, column = @current_pos
+    column = column + 1
+    return if column >= keypad[row].length || keypad[row][column].nil?
+    @current_pos = [row, column]
   end
 
-  def move_D
-    return if BOTTOM.include?(@current_pos)
-    @current_pos = @current_pos + 3
+  def move_D(keypad)
+    row, column = @current_pos
+    row = row + 1
+    return if row >= keypad.length || keypad[row][column].nil?
+    @current_pos = [row, column]
   end
 
-  def move_U
-    return if TOP.include?(@current_pos)
-    @current_pos = @current_pos - 3
+  def move_U(keypad)
+    row, column = @current_pos
+    row = row - 1
+    return if row == -1 || keypad[row][column].nil?
+    @current_pos = [row, column]
   end
 
 end
